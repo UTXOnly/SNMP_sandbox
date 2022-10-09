2ccf2b5988b76e21bfdb414bbb306f826b739129
UTXOnly <brian.hartford@datadoghq.com>
2022-10-09 14:23:48 -0400

    Added snmp docker config folder

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ source ~/vagrant/.sandbox.conf.sh
 
 #PATH_TO_SNMP="./sample_custom_profile.yaml"
 #CONF_SNMP="./conf.yaml"
-DD_API_KEY=696d1f09a9b63b037105225c7790bb0a
+#DD_API_KEY=
 echo api key ${DD_API_KEY}
 echo "Installing Datadog Agent 7 from api_key: ${DD_API_KEY} but not starting it yet"
 
@@ -22,12 +22,14 @@ export LC_ALL=en_US.UTF-8
 
 echo "Set log level of the datadog agent to TRACE"
 echo 'log_level: trace'| sudo tee -a /etc/datadog-agent/datadog.yaml
+echo 'dogstatsd_non_local_traffic: true' | sudo tee -a /etc/datadog-agent/datadog.yaml
+#echo 'logs_enabled: true' | sudo tee -a /etc/datadog-agent/datadog.yaml
 echo "Adding custom SNMP profile"
 touch /etc/datadog-agent/conf.d/snmp.d/profiles/custom_profile.yaml
 touch /etc/datadog-agent/conf.d/snmp.d/conf.yaml
 #echo ${PATH_TO_SNMP} | sudo tee -a /etc/datadog-agent/conf.d/snmp.d/profiles/custom_profile.yaml
 #echo ${CONF_SNMP} | sudo tee -a touch /etc/datadog-agent/conf.d/snmp.d/conf.yaml
-sudo tee >/etc/datadog-agent/conf.d/snmp.d/profiles/custom_profile.yaml <<EOF
+sudo tee >/etc/datadog-agent/conf.d/snmp.d/profiles/_custom_profile.yaml <<EOF
 extends:
   - _cisco-generic.yaml
 
@@ -49,26 +51,25 @@ sudo tee >/etc/datadog-agent/conf.d/snmp.d/conf.yaml <<EOF
 init_config:
 
 
-    loader: core
+  loader: core
 
     
-    use_device_id_as_hostname: true
+  use_device_id_as_hostname: true
 
     
-    profiles:
-      custom_test:
-        definition_file: custom_profile.yaml
+  profiles:
+    custom_test:
+      definition_file: custom_profile.yaml
     
 
 
 instances:
 
-  - ip_address: 127.0.0.1
-    profile: custom_test
+  - ip_address: 192.168.1.153
     snmp_version: 2
     loader: core
 
-    community_string: ipsec
+    community_string: cisco_asa_5525
     
 
     
@@ -77,8 +78,8 @@ EOF
 
 
 
-echo "Install maven"
-sudo apt install -y maven
+#echo "Install maven"
+#sudo apt install -y maven
 
 echo "Start Datadog agent"
 #sudo systemctl start datadog-agent
