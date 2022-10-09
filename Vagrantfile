b638c881fe37983868d4c13265ee8e7384bb7b2c
Brian Hartford <bhartford419@gmail.com>
2022-10-09 11:18:37 -0400

    config updates

--- Vagrantfile
+++ Vagrantfile
@@ -1,11 +1,11 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "uwbbi/bionic-arm64"
+  config.vm.box = "ubuntu/trusty64"
 
   #config.vm.provider "vmware_fusion" do |v|
-  config.vm.provider 'vmware_fusion' do |v|
+  config.vm.provider 'virtualbox' do |v|
   end
 
   config.vm.synced_folder "./data", "/data", create: true
-  config.vm.provision :file, source: '~/.sandbox.conf.sh', destination: '/home/vagrant/.sandbox.conf.sh'
-  config.vm.provision "shell", path: "./setup.sh"
+  config.vm.provision :file, source: '~/.sandbox.conf.sh', destination: '~/vagrant/.sandbox.conf.sh'
+  config.vm.provision "shell", path: "setup.sh"
 end
--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-source /~/vagrant/.sandbox.conf.sh
+source ~/vagrant/.sandbox.conf.sh
 
-$1 = PATH_TO_SNMP
-$2 = CONF_SNMP
+#PATH_TO_SNMP="./sample_custom_profile.yaml"
+#CONF_SNMP="./conf.yaml"
+DD_API_KEY=696d1f09a9b63b037105225c7790bb0a
 echo api key ${DD_API_KEY}
 echo "Installing Datadog Agent 7 from api_key: ${DD_API_KEY} but not starting it yet"
 
-DD_INSTALL_ONLY=true DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=${DD_API_KEY}  bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
-
+DD_INSTALL_ONLY=true DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=${DD_API_KEY} DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 echo "Update apt-get"
 sudo apt-get update
 
@@ -15,12 +15,65 @@ echo "Fix lang issue... + export LC_ALL so we don't have to logout/login"
 echo 'LC_ALL=en_US.UTF-8'| sudo tee -a /etc/default/locale
 export LC_ALL=en_US.UTF-8
 
+#PATH_TO_SNMP=<
+
+
+#CONF_SNMP=
+
 echo "Set log level of the datadog agent to TRACE"
 echo 'log_level: trace'| sudo tee -a /etc/datadog-agent/datadog.yaml
 echo "Adding custom SNMP profile"
-cat ${PATH_TO_SNMP} | sudo tee -a /etc/datadog-agent/snmp.d/profiles/custom_profile.yaml
-cat ${CONF_SNMP} | sudo tee -a /etc/datadog-agent/snmp.d/conf.yaml
+touch /etc/datadog-agent/conf.d/snmp.d/profiles/custom_profile.yaml
+touch /etc/datadog-agent/conf.d/snmp.d/conf.yaml
+#echo ${PATH_TO_SNMP} | sudo tee -a /etc/datadog-agent/conf.d/snmp.d/profiles/custom_profile.yaml
+#echo ${CONF_SNMP} | sudo tee -a touch /etc/datadog-agent/conf.d/snmp.d/conf.yaml
+sudo tee >/etc/datadog-agent/conf.d/snmp.d/profiles/custom_profile.yaml <<EOF
+extends:
+  - _cisco-generic.yaml
 
+metrics:
+- MIB: CISCO-IPSEC-FLOW-MONITOR-MI
+  forced_type: monotonic_count_and_rate
+  symbols:
+  - OID: 1.3.6.1.4.1.9.9.171.1.3.2.1.26
+    name: cipSecTunInOctets
+  - OID: 1.3.6.1.4.1.9.9.171.1.3.2.1.39
+    name: cipSecTunOutOctets
+  - OID: 1.3.6.1.4.1.9.9.171.1.3.2.1.33
+    name: cipSecTunInDropPkts
+  - OID: 1.3.6.1.4.1.9.9.171.1.3.2.1.46
+    name: cipSecTunOutDropPkts
+EOF
+
+sudo tee >/etc/datadog-agent/conf.d/snmp.d/conf.yaml <<EOF  
+init_config:
+
+
+    loader: core
+
+    
+    use_device_id_as_hostname: true
+
+    
+    profiles:
+      custom_test:
+        definition_file: custom_profile.yaml
+    
+
+
+instances:
+
+  - ip_address: 127.0.0.1
+    profile: custom_test
+    snmp_version: 2
+    loader: core
+
+    community_string: ipsec
+    
+
+    
+    
+EOF
 
 
 
@@ -28,4 +81,5 @@ echo "Install maven"
 sudo apt install -y maven
 
 echo "Start Datadog agent"
-sudo systemctl start datadog-agent
+#sudo systemctl start datadog-agent
+sudo service datadog-agent start
