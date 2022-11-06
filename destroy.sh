943bb5bba6a97b2d0f28a53d8144a03419a20fbf
UTXOnly <brian.hartford@datadoghq.com>
2022-11-05 21:00:42 -0400

    intial commit with tcpdump

--- destroy.sh
+++ destroy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-docker kill datadog/agent
-docker kill tandrup/snmpsim
+docker kill datadog-agent
+docker kill dd-snmp
 docker network rm test-net
 docker image rm datadog/agent -f
 docker image rm tandrup/snmpsim -f
+docker image rm snmp-datadog -f
+
