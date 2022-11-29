3b39df1949688c9b410df4be2764aa13b8487f9b
UTXOnly <brian.hartford@datadoghq.com>
2022-11-28 19:28:57 -0500

    Corrected Docker exec syntax for tcp dump,now working

--- run.sh
+++ run.sh
@@ -15,11 +15,11 @@ echo "Docker up"
 #mkdir ./"status&tcpdumps"
 echo "################## TCPDUMP started, please wait 30 seconds #######################################"
 #Starting a tcpdump filtering traffic on port 161 to closer inspect 
-docker exec datadog-agent tcpdump -G 30 port '(161 or 8125)' -W 1 -w ./"status&tcpdump"/dump$(date +'%m-%d-%Y').pcap
+docker exec datadog-agent tcpdump -G 10 port '(161 or 8125)' -W 1 -w /tcpdumps/dump$(date +'%m-%d-%Y').pcap
 
 echo "################### Running SNMP check ####################################"
 
 #docker exec datadog-agent touch /tcpdumps/debug_snmp_check.txt && \
 #touch ./"status&tcpdump"/debug_snmp_check.log
-docker exec "datadog-agent agent check snmp -l debug" > ./"status&tcpdump"/debug_snmp_check.log
+docker exec datadog-agent bash -c 'agent check snmp -l debug > /tcpdumps/debug_snmp_check.log'
 
