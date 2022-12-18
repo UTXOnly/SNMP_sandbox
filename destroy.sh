9daf9089610d359bc01794e8054b352bc63f7798
Brian Hartford <bhartford419@gmail.com>
2022-12-18 17:49:01 -0500

    added mac sed commands, mib2snmprec

--- destroy.sh
+++ destroy.sh
@@ -7,3 +7,6 @@ docker stop $(docker ps -a -q)
 docker rmi $(docker image ls -a -q) -f
 sudo rm -r ./snmp/tcpdump
 sudo rm -r ./parsed_yaml
+#sed "s/$/${MIB_NAME}/" -i /conversion/.env
+sed -i '72,$d' ./snmp/docker-compose.yaml
+#sed -i.bak '5,$d' file.txt
