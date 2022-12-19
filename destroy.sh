1e11dc8b2301d0325a6148049647f07a785f7fc6
Brian Hartford <bhartford419@gmail.com>
2022-12-18 20:42:59 -0500

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-source ./snmp/.env
 
-docker-compose down --rmi all --remove-orphans
+docker-compose down --remove-orphans
 docker stop $(docker ps -a -q)
 #Only uncomment if you want REMOVE ALL DOCKER IMAGES!!!
-docker rmi $(docker image ls -a -q) -f
+#docker rmi $(docker image ls -a -q) -f
 sudo rm -r ./snmp/tcpdump
 sudo rm -r ./parsed_yaml
-#sed "s/$/${MIB_NAME}/" -i /conversion/.env
 sed -i '72,$d' ./snmp/docker-compose.yaml
 sed -r -i '' '72,$d' ./snmp/docker-compose.yaml
-#sed -i.bak '5,$d' file.txt
