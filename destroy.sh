55e5cc4cf0d39d5c26f91feddd1408cddd5da99d
UTXOnly <brian.hartford@datadoghq.com>
2022-12-18 22:54:37 -0500

    Update mac commands to destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -3,8 +3,15 @@
 docker-compose down --remove-orphans
 docker stop $(docker ps -a -q)
 #Only uncomment if you want REMOVE ALL DOCKER IMAGES!!!
-docker rmi $(docker image ls -a -q) -f
+#docker rmi $(docker image ls -a -q) -f
 sudo rm -r ./snmp/tcpdump
 sudo rm -r ./parsed_yaml
-sed -i '72,$d' ./snmp/docker-compose.yaml
-sed -r -i '' '72,$d' ./snmp/docker-compose.yaml
+if [ "$(uname)" == "Darwin" ]; then
+    #Mac branch
+    sed -r -i '' '72,$d' ./snmp/docker-compose.yaml
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Linux branch
+    sed -i '72,$d' ./snmp/docker-compose.yaml
+else
+    echo -e "${BRed}\nUnable to detect OS...skipping sed substituion${NC}"
+fi
