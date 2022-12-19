ee5bd37ac2ef959f56c78099d3f3a548a1d7e182
UTXOnly <brian.hartford@datadoghq.com>
2022-12-18 22:34:12 -0500

    Adjust destroy file

--- destroy.sh
+++ destroy.sh
@@ -3,7 +3,7 @@
 docker-compose down --remove-orphans
 docker stop $(docker ps -a -q)
 #Only uncomment if you want REMOVE ALL DOCKER IMAGES!!!
-#docker rmi $(docker image ls -a -q) -f
+docker rmi $(docker image ls -a -q) -f
 sudo rm -r ./snmp/tcpdump
 sudo rm -r ./parsed_yaml
 sed -i '72,$d' ./snmp/docker-compose.yaml
